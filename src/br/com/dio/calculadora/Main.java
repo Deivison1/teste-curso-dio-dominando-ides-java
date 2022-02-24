47fec621ebdfe1a43f8fca270292673bcbc95553
Deivison1 <santosdeivison480@gmail.com>
2022-02-24 12:39:51 -0300

    exemplos de variaveis

---
+++ src/br/com/dio/calculadora/Main.java
@@ -0,0 +1,46 @@
+package br.com.dio.calculadora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+     int i;
+     //int i;
+     int I;
+     //int 1i;
+     int _ia;
+     int $ag;
+     
+     i  = 5;
+     I  = 10;
+    _ia = 20;
+     $ag =7;
+     
+     final int j = 10;
+     
+     //expressividade das variáveis 
+     
+     int quantidadeProduto = 50;
+     
+     final int NUMERO_TENTATIVAS = 5;
+     
+     //final int numeroTentativas = 5;
+     
+     //int QUANTIDADE_OPCOES = 25;
+     
+     //int qtdProd;
+     
+     System.out.println(i);
+     System.out.println(I);
+     System.out.println(_ia);
+     System.out.println($ag);
+     
+     System.out.println(j);
+     System.out.println(quantidadeProduto);
+     System.out.println(NUMERO_TENTATIVAS);
+     
+    
+}
+	
+	
+}
