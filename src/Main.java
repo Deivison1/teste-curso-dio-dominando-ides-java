305489e63fe2f2544313f4eb55b1331e0946c827
Deivison1 <santosdeivison480@gmail.com>
2022-02-26 09:58:21 -0300

    atualizando

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,31 @@ public class Main {
 		
       //Calculadora
 		
-		System.out.println("Wxercício claculadora");
+		System.out.println("Exercício claculadora");
+		System.out.println("");
 		
 	    Calcular.soma(9, 4);
 	    Calcular.subtracao(9, 1.8);
 	    Calcular.multiplicacao(5, 5);
 	    Calcular.divisao(25, 3);
-		
+	    System.out.println("");
+	    
 	    // Mensagem
 	    System.out.println("Exercicio mensagen");
+	    System.out.println("");
 	    
 	    Mensagem.obeterMensagem(9);
 	    Mensagem.obeterMensagem(14);
 	    Mensagem.obeterMensagem(1);
+	    System.out.println("");
 	    
 	    // Emprestimos
+	    System.out.println("Emprestimos");
+	    System.out.println("");
 	    
-	    Emprestimo.calcular( 1000, Emprestimo.getDuasParcelas());
-	    Emprestimo.calcular( 1000, Emprestimo.getTaxasTresParcelas());
-	    
-	}
+	    Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+}
 
 }
