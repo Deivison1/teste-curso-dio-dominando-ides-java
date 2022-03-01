e5203f8ffc18ec009bf9e92d280a1832065ab8f2
Deivison1 <santosdeivison480@gmail.com>
2022-03-01 09:52:41 -0300

    exercícios

---
+++ src/br/com/dio/exercicios/loops/Ex_Tabuada.java
@@ -0,0 +1,34 @@
+package br.com.dio.exercicios.loops;
+/*
+  Desenvolva um gerador de tabuada, capaz de  gerar a tabuada de qualquer npumero inteiro entre 1 a 10.
+  O usuáro deve informar de qual numero ele deseja ver a tabauada.
+  A saída deve ser conforme o exemplo abaixo:
+  
+  *Tabuada de 5;
+  *
+  *5 X 1 = 5
+  *5 X 2 = 10
+  *....
+  *5 X 10 = 50
+  */
+import java.util.Scanner;
+
+public class Ex_Tabuada {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Tabuada: ");
+		
+		int tabuada = scan.nextInt();
+
+		System.out.println("Tabuada: " + tabuada);
+		
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+			
+		}
+	}
+
+}
