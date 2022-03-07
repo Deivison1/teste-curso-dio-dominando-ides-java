121b3ddb5fc52faf1e4d2cdbf79be8301aa03ed9
Deivison1 <santosdeivison480@gmail.com>
2022-03-07 16:48:45 -0300

    Criando Associacao

--- src/br/com/dio/exercicios/poo/ClasseFilha.java
+++ src/br/com/dio/exercicios/poo/ClasseFilha.java
@@ -4,7 +4,7 @@ public class ClasseFilha extends ClasseMae {
 
 	@Override
 	void metodo1() {
-		System.out.println("Método 1 da clase filha");
+		System.out.println("Método 1 da clase Filha");
 	}
 	
 }
--- src/br/com/dio/exercicios/poo/associacao/Pessoa.java
+++ src/br/com/dio/exercicios/poo/associacao/Pessoa.java
@@ -2,4 +2,5 @@ package br.com.dio.exercicios.poo.associacao;
 
 public class Pessoa {
 
+	Endereco endereco;
 }
